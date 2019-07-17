2edc5dd19c507956ddd286aaf830548b19e82ea8
seleuchel <smsky1010@gmail.com>
2019-07-17 22:21:51 +0900

    참고파일 일부수정

--- 참고_리눅스병합_대략코드/Analyze_ELF.py
+++ 참고_리눅스병합_대략코드/Analyze_ELF.py
@@ -1,9 +1,9 @@
-import resultDataFrame
+#-*-coding: utf-8-*-
+from Result_DataFrame import *
 '''
 ELF 분석 엔진
 - analyze_ELF_32(filename,binary) : 결과를 저장할 데이터 프레임 생성
 - analyze_ELF_64(filename, binary)
-- devide_binary(binary) : 바이너리를 나눔
 
 test
 #check memory protector
@@ -13,7 +13,7 @@ test
 - is_RELRO
 
 #1. 결과를 저장할 데이터 프레임 생성
-#2. 바이너리를 나눔 <- 분석과정이 필요하지 않을 수도 있다(오픈소스 분석 필요)
+#2.  분석(오픈소스 필요)
 #3. 적용된 메모리 보호 기법 분석
     #1.1)CANARY
     #1.2)NX
@@ -24,55 +24,62 @@ test
     #2)데이터프레임 반환
 
 '''
-def analyze_ELF_32(filename,binary):
+def analyze_ELF_32(filename):
     columns=['Filename', 'CANARY', 'NX', 'PIE', 'RELRO']
 
     #결과를 저장할 데이터 프레임 생성
-    resultTable = result_DataFrame()
+    resultTable = Result_DataFrame()
     resultTable.create_DataFrame(columns)
 
-    #바이너리를 나눔
-    Header, CodeSeg, DataSeg, ExtraInfo = devide_binary(binary)
+    #파일을 불러옴 (rb)
+    #일정 부분을 어떻게 가져와 처리
+    binary = 0x30
 
     #적용된 메모리 보호 기법 분석 (예시. 추가 조사 필요)
     resultlist=[]
     resultlist.append(filename)
-    resultlist.append((lambda x : 'O' if x else 'X')(is_CANARY(Header)))
-    resultlist.append((lambda x : 'O' if x else 'X')(is_NX(Header)))
-    resultlist.append((lambda x : 'O' if x else 'X')(is_PIE(Header)))
+    resultlist.append((lambda x : 'O' if x else 'X')(is_CANARY(binary)))
+    resultlist.append((lambda x : 'O' if x else 'X')(is_NX(binary)))
+    resultlist.append((lambda x : 'O' if x else 'X')(is_PIE(binary)))
     resultlist.append(is_CANARY(is_RELRO))
 
     #데이터프레임에 결과를 저장 및 return
 # 논의 : 데이터프레임을 넘기는 것이 좋을지, resultTable 객체를 넘기는 것이 좋을 지 모르겠다.
 
-    resultlist.add_row(resultlist)
+    resultTable.add_row(resultlist)
     return resultTable
 
 
-def analyze_ELF_64(filename, binary):
+def analyze_ELF_64(filename):
     pass
 
-#divide binary
-def devide_binary(binary):
-    # 분석에 필요한 바이너리 부분만 나누어 잘라 return
-    return Header, CodeSeg, DataSeg, ExtraInfo
+
 
 '''
 #check memory protector
 #분석에 필요한 바이너리 부분을 받아 사용
 '''
-def is_CANARY(bipart):
+def is_CANARY(binary):
     #return bool
-    pass
+    return True
 
-def is_NX(bipart):
+def is_NX(binary):
     #return bool
-    pass
+    return True
 
-def is_PIE(bipart):
+
+def is_PIE(binary):
     #return bool
-    pass
+    return True
 
-def is_RELRO(bipart):
+def is_RELRO(binary):
     #return string (3 type)
-    pass
+    return "RELRO"
+
+
+##############testcode##############################
+if __name__ == '__main__':
+    ##main 함수에 추가해 주세요
+    DataFrame = analyze_ELF_32('samplecode')
+    Datas=DataFrame.get_DataFrame()
+    print(Datas)
