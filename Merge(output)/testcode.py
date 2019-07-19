9e6e9448b5c77a76eadb037644a04026ec8967a7
seleuchel <37205198+seleuchel@users.noreply.github.com>
2019-07-19 18:33:16 +0900

    Update testcode.py

--- Merge(output)/testcode.py
+++ Merge(output)/testcode.py
@@ -1,8 +1,8 @@
-'''
-Merge(input)`s test maincode
-'''
-import Analyze_PE
+#-*-coding: utf-8-*-
+#engine - merge(output) testcode
 import Analyze_ELF
+import Result_DataFrame
+from OutputTest import *
 
 
 def engine(signature, filename):
@@ -22,33 +22,20 @@ def engine(signature, filename):
 #            pass#return Analyze_PE.analyze_PE_64(filename)
 
 
-
 def main():
-    #파일 이름을 인자로 받음
+    signature = True
+    filename = "samplefile"
 
+    #analysis result
+    DataFrame = engine(signature, filename)
 
-    #옵션 추출하기
-        #-j : json
-        #-c : csv
-        #-p : console
-
-
-    #파일 가져오기
-
-
-    #파일 열어서 (rb) 앞부분 시그니처 분석
-        # readelf 시 elf32, elf64, pe32, pe64를 검사할 수 있는부분
-        # +)앞 부분의 시그니처만 검사해 판단한다면, 해당 위치의 바이트를 조작했을 때, 오류가 일어날 수 있습니다
-        #elf32, elf64, pe32,pe64를 구분할 수 있는 또 다른 방법을 찾아 추가하면 좋을 것 같습니다.
-
-
-    #engine 함수 완성
-
-
-    #디버그용 코드도 추가 바랍니다
-        #1. 인자로 파일명과 옵션을 잘 받아오는지 출력
-        #2. 파일 형식 바이너리 출력
-
+    #output
+    #-j : json
+    output('-j', DataFrame)
+    #-c : csv
+#    output('-c', DataFrame)
+    #-p : console
+#    output('-p', DataFrame)
 
 
 if __name__ == '__main__':
