366e9012f1a41eef49fd10449b65185f288849c9
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-07-19 18:18:51 +0900

    Update result_output.py
    Edit indentation and line 32

--- result_output.py
+++ result_output.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import json
 
-columnString = ['file_name', 'DotNET', 'NX', 'SEH']
-
 ############################################################
 class Result_DataFrame:
     def __init__(self):
@@ -32,22 +30,21 @@ class Result_DataFrame:
 ################dataframe class##############################
 
 def output(opt,DataFrame):
-    def output(opt, DataFrame):
+    Datas = DataFrame.get_DataFrame()
+    if opt == '-j':
+        jstring = json.dumps(Datas.to_json(orient='split'), indent=4)
+        with open('result_Json.json', 'w') as jsonfile:
+            jsonfile.write(jstring)
+
+    elif opt == '-c':
+        Datas.to_csv('result_Csv.csv')
+
+    elif opt == '-p':
         Datas = DataFrame.get_DataFrame()
-        if opt == '-j':
-            jstring = json.dumps(Datas.to_json(orient='split'), indent=4)
-            with open('result_Json.json', 'w') as jsonfile:
-                jsonfile.write(jstring)
+        print(Datas)
 
-        elif opt == '-c':
-            Datas.to_csv('result_Csv.csv')
-
-        elif opt == '-p':
-            Datas = DataFrame.get_DataFrame()
-            print(Datas)
-
-        else:
-            print('wrong input')
+    else:
+        print('wrong input')
 
 if __name__ == "__main__":
     #######testData#######
