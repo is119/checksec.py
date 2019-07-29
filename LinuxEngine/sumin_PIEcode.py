7fda623318d83d8d3a8bbe558569e8b719c192df
seleuchel <smsky1010@google.com>
2019-07-30 00:06:58 +0900

    ver1_PIE

--- LinuxEngine/sumin_PIEcode.py
+++ LinuxEngine/sumin_PIEcode.py
@@ -1,14 +1,4 @@
-from Result_DataFrame import *
 from elftools.elf.elffile import ELFFile
-from elftools.elf.descriptions import *
-from elftools.elf.structs import *
-from elftools.elf.dynamic import *
-from elftools.elf.sections import *
-from elftools.elf.enums import ENUM_D_TAG
-from elftools.elf.segments import *
-from elftools.elf.descriptions import _DESCR_D_TAG, _low_priority_D_TAG
-import sys
-import os
 
 def main():
     #if you want to test, change file route
@@ -28,7 +18,8 @@ def main():
         while i < elf_section_dynamic.num_tags():
             dynamic_entry = str(elf_section_dynamic.get_tag(i))
             if 'DT_DEBUG' in dynamic_entry:
-                print(dynamic_entry,'PIE')
+                # debug : print(dynamic_entry)
+                print('PIE')
                 break
             else:
                 i += 1
