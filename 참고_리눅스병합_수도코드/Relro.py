19be0dd23338099a57523502492f873cc4fd73d9
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2019-08-02 01:41:31 +0900

    Final Relro
    full relro & partial relro & no relro sample test 완료했습니다!
    
    진짜 안될 수가 없다 진짜 안되면 밥 사요!

--- 참고_리눅스병합_수도코드/Relro.py
+++ 참고_리눅스병합_수도코드/Relro.py
@@ -1,39 +1,38 @@
-
 from elftools.elf.elffile import *
 from elftools.elf.dynamic import *
+
 def main():
 
-    f = open('D:\\2019_KUCIS_Project_checksec.py\\참고_리눅스병합_수도코드\\relro2','rb')
+    f = open('D:\\2019_KUCIS_Project_checksec.py\\참고_리눅스병합_수도코드\\relro_full32','rb')
     elf = ELFFile(f)
 
     # Is it relro?
-    have_Relro = False
+
+    # elf 파일 내 segment 리스트 저장
     for segment in elf.iter_segments():
-        if "PT_GNU_RELRO" in segment['p_type']:
-            return print('X')
-        else:
-            have_Relro = True
-            break
+        have_segment = segment['p_type']
 
-    # FULL RELRO vs PARTAL RELRO
+    # Relro vs No Relro
 
+    have_Relro = False
+    if "PT_GNU_RELRO" in have_segment:
+        have_Relro = True
+    else:
+        return print('X')
+
+    # FULL RELRO vs PARTAL RELR
+    key = "DT_BIND_NOW"
     if have_Relro:
-        key = "DT_BIND_NOW"
         for section in elf.iter_sections():
+            # print(DynamicSection)
             if type(section) is DynamicSection:
                 for tag in section.iter_tags():
                     if tag.entry.d_tag == key:
-                        return print('O(Full Relro)')
+                         whatrelro = 'O(Full Relro)'
+                         break
                     else:
-                        return print('O(Partial Relro)')
-
-    # for section in elf.Dynamicsection:
-    #     print(section)
-    #     for tag in section.iter_tags():
-    #         if tag.entry.d_tag == key:
-    #             return print('O(Full Relro)')
-    #         else:
-    #             return print('O(Partial Relro)')
+                         whatrelro ='O(Partial Relro)'
+    return print(whatrelro)
 
     f.close()
 
