13ee8d14cd9092769c1b0022a9ce4c8bfe9b18c1
seleuchel <smsky1010@gmail.com>
2019-11-07 21:55:52 +0900

    All_edit

--- Merge(input)/Analyze_PE.py
+++
@@ -1,7 +0,0 @@
-# this file is debug code!!
-
-def analyze_PE_32(filename):
-    print(filename)
-
-def analyze_PE_64(filename):
-    print(filename)
--- sample/PIE/DSO/readme.txt
+++
@@ -1,3 +0,0 @@
-dso는 
-pie 옵션을 주든, 제거하든 둘 다 동일하게 DSO가 출력됨
-sample code (elf relocatable file) # no executable
