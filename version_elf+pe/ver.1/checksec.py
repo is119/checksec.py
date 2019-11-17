d81ef91ce2c4c266006de5fd36cfa36b443208de
koyo <koyo@koyo.kr>
2019-11-17 23:48:01 +0900

    Create os_check.py for windows

--- version_elf+pe/ver.1/checksec.py
+++ version_elf+pe/ver.1/checksec.py
@@ -1,12 +1,14 @@
 import csv
 import json
 from argparse import ArgumentParser
+from platform import platform
 
 import magic
 from tabulate import tabulate
 
 import Analyze_ELF
 import Analyze_PE
+import os_check
 
 
 def engine(file_path):
@@ -26,18 +28,18 @@ def engine(file_path):
         #edit-sumin test code
         # authmem(file_path)
         return Analyze_PE.analyze_PE(file_path)
+    else:
         raise AttributeError("Not Executable File : '%s'" % file_path)
 
 
 def parse_args():
-    parser = ArgumentParser(
-        description='Check Security of Excutables')
+    parser = ArgumentParser(description='Check Security of Excutables')
     g = parser.add_mutually_exclusive_group()
     g.add_argument('-c', '--csv', dest='csv', action='store_true', default=False, help='save result as csv')
     g.add_argument('-j', '--json', dest='json', action='store_true', default=False, help='save result as json')
     parser.add_argument('-o', '--os', dest='os', action='store_true', default=False, help='check os security')
     parser.add_argument('-b', '--build', dest='build', action='store_true', default=False, help='check build information')
-    parser.add_argument('file_paths', metavar='file_path', nargs='+')
+    parser.add_argument('file_paths', metavar='file_path', nargs='*')
     return parser.parse_args()
 
 
@@ -46,7 +48,7 @@ def main():
 
     os_result = {}
     if args.os:
-        pass
+        os_result = os_check.analyze_system()
 
     results_by_file = {}
     for file_path in args.file_paths:
@@ -57,12 +59,12 @@ def main():
             results['build'] = {}
 
     if args.os:
-        print(
-            tabulate([os_result.keys(), os_result.values()], tablefmt='plain'))
+        print('os:', platform())
+        print(tabulate([os_result.keys(), os_result.values()], tablefmt='plain'))
         print()
 
     for file_path, results in results_by_file.items():
-        print('filename: %s' % file_path)
+        print('filename:', file_path)
         for r in results.values():
             print(tabulate([r.keys(), r.values()], tablefmt='plain'))
         print()
